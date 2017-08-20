98e63d0649a271b145515cd114297fa45f1f2bc9
Trai Compton <tlcompton2546@outlook.com>
2017-08-20 19:15:23 +0000

    Created view files and skeleton code for controller of the EntityModel

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,30 @@
 class EntitiesController < ApplicationController
+  before_action :set_entity, except: [:index, :new, :create]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_entity
+    @entity = Entity.find(params[:id])
+  end
 end
